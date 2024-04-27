cdcac7e9b212ac224cb0ca5d9691a46f36e31fa6
mara <mara@the-witch.ru>
2024-04-27 23:39:11 +0300

    remove smaller bigger

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/arch/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/arch/keymap.c
@@ -163,8 +163,6 @@ enum my_keycodes {
 #define PrevApp LALT(KC_TAB)
 
 #define Tmux LCTL(KC_A)
-#define Smaller LCTL(Minus)
-#define Bigger LCTL(Equal)
 
 #define MakeFloat LCAG(KC_C)
 #define MoveFloat LCAG(KC_D)
@@ -250,7 +248,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_APP] = LAYOUT_ortho_4x12_1x2uC(
     _, PrntSc1, PrntSc2, PrntSc3, _, _, _, _, _, _, _, _,
     _, _, LightDec, LightInc, _, _, _, _, Menu, Buffer, Pass, _,
-    _, _, SoundDec, SoundInc, _, _, _, _, Smaller, Bigger, _, _,
+    _, _, SoundDec, SoundInc, _, _, _, _, _, _, _, _,
     _, _, _, _,    _,   __, _, _, _, _, _
 ),
 [_APP2] = LAYOUT_ortho_4x12_1x2uC(
