00e85b765b555fc52d60b6afa2e638c1bab65f7b
mara <mara@the-witch.ru>
2024-05-14 00:10:00 +0300

    t

--- keyboards/kprepublic/bm40hsrgb/rev2/keymaps/arch/keymap.c
+++ keyboards/kprepublic/bm40hsrgb/rev2/keymaps/arch/keymap.c
@@ -251,7 +251,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_APP] = LAYOUT_ortho_4x12_1x2uC(
     _, _, SoundDec, SoundInc, _, _, _, _, PrntSc1, PrntSc2, PrntSc3, _,
     _, RGB_TOG, LightDec, LightInc, _, _, _, _, _, _, _, _,
-    _, _, _, _, _, _, _, _, WS_1, WS_2, WS_3, _,
+    _, _, _, _, _, _, _, _, _, _, _, _,
     _, _, _, _, _, __, _, _, _, QK_BOOT, KC_KB_POWER
 ),
 [_NAVIGATION] = LAYOUT_ortho_4x12_1x2uC(
@@ -262,8 +262,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_TG] = LAYOUT_ortho_4x12_1x2uC(
     _, Monitor1, SCUp, Monitor2,         _, _, _, _,   _, _, _, _,
-    _, SCLeft, SCDown, SCRight,   _, _, _, _,   _, _, _, _,
-    _, _, _,      _,      Vpn,         _, _,   _, _, _, _, _,
+    Fullscreen, SCLeft, SCDown, SCRight,   _, _, _, _,   _, _, _, _,
+    _, WS_1, WS_2, WS_3, Vpn,         _, _,   _, _, _, _, _,
     _, _, _,      _,      _,          __,    _, _, _, _, _
 ),
 // [_RECTANGLE] = LAYOUT_ortho_4x12_1x2uC(
